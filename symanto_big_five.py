e12b99cdb0e6a2d73208a504cbdd589d9d0840c9
StephanieMalvicini <s.a.m_puan@hotmail.com>
2023-12-27 14:30:39 +0100

    Get big five in batches

--- symanto_big_five.py
+++ symanto_big_five.py
@@ -1,55 +1,75 @@
+import json
 import time
+
 import pandas as pd
 import requests
-import json
+import winsound
 
 with open("config.json", "r") as config_file:
     config = json.load(config_file)
 
 BIG_FIVE_RESULTS = 'outputs/personality/symanto_big_five_{0}.csv'.format(time.time())
-USERS_MESSAGES_FILE = 'dataset/network_users_tweets_to_analyze.csv' # TODO: use network_users_tweets (renamed)
+USERS_MESSAGES_FILE = 'dataset/network_users_tweets_to_analyze.csv'
 SYMANTO_BIG_FIVE_URL = "https://big-five-personality-insights.p.rapidapi.com/api/big5"
 
 
-def get_symanto_big_five(user_data):
-    payload = [
-        {
-            "id": str(user_data['id']),
-            "language": "en", # We have mixed languages on the dataset.
-            # TODO: check if we need to filter the non english parts of the tweets or what
-            "text": str(user_data['tweet_content'])
-        }
-    ]
+def payload(users):
+    payload = []
+    for index, user in users.iterrows():
+        user_payload = {
+                "id": str(user['id']),
+                "language": "en",
+                "text": str(user['tweet_content'])
+            }
+        payload.append(user_payload)
+    return payload
+
+
+def get_symanto_big_five(users):
     headers = {
         "content-type": "application/json",
         "X-RapidAPI-Key": config["rapidAPI-Key"],
         "X-RapidAPI-Host": "big-five-personality-insights.p.rapidapi.com"
     }
-    response = requests.post(SYMANTO_BIG_FIVE_URL, json=payload, headers=headers)
+
+    response = requests.post(SYMANTO_BIG_FIVE_URL, json=payload(users), headers=headers)
+    error_streak = 0
+    sleep_time = 0
+
+    while response.status_code != 200 and error_streak < 3:
+        print(' ---> Error steak: {0}'.format(error_streak))
+        print(' ---> Error {0} - {1}'.format(response.status_code, response.reason))
+        error_streak += 1
+        sleep_time += 1
+        time.sleep(sleep_time)
+        response = requests.post(SYMANTO_BIG_FIVE_URL, json=payload(users), headers=headers)
+
     return response
 
 
 user_messages = pd.read_csv(USERS_MESSAGES_FILE)
-successful_users_indexes = []
-error_strikes = 0
 big_five_results_rows = []
+start = 142900
+steps = 1000
+end = start + steps
 
-for index, user in user_messages.iterrows():
+while len(user_messages[start:end] != 0):
     try:
-        result = get_symanto_big_five(user)
-        big_five_results_rows.append(result.json()[0])
-        successful_users_indexes.append(index)
+        print('Processing user messages[{0}:{1}]'.format(start, end))
+        results = get_symanto_big_five(user_messages[start:end])
+        for result in results.json():
+            big_five_results_rows.append(result)
+        print('Done processing user messages[{0}:{1}]'.format(start, end))
     except Exception as e:
         print(e)
-        error_strikes += 1
-    if error_strikes > 2:
+        print('Start: {0}'.format(start))
+        print('End: {0}'.format(end))
         break
+    start = end
+    end += steps
+
 
 # Save results
 big_five_results = pd.DataFrame(big_five_results_rows)
 big_five_results.to_csv(BIG_FIVE_RESULTS, index=False)
-
-# Leave only the remaining users on the file
-user_messages = user_messages.drop(index=successful_users_indexes)
-user_messages.to_csv(USERS_MESSAGES_FILE, index=False)
-
+winsound.Beep(440, 1000)
