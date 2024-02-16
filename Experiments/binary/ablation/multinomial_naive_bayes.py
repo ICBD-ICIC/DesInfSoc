153a463caad649821940e64ef72b6963be59e3c6
StephanieMalvicini <s.a.m_puan@hotmail.com>
2024-02-16 14:56:05 +0100

    Ablation, no personality

---
+++ Experiments/binary/ablation/multinomial_naive_bayes.py
@@ -0,0 +1,34 @@
+import base_model
+from sklearn.naive_bayes import MultinomialNB
+import time
+import json
+
+start = time.time()
+
+X_train, X_test, y_train, y_test = base_model.get_train_test_split()
+
+BEST_ATTRIBUTES = {
+    '28': {'fit_prior': False, 'alpha': 0.2},
+    '30': {'fit_prior': False, 'alpha': 0.2},
+    '31': {'fit_prior': False, 'alpha': 0.2},
+    '32': {'fit_prior': False, 'alpha': 0.2},
+    '34': {'fit_prior': True, 'alpha': 0},
+    '36': {'fit_prior': False, 'alpha': 0.2}
+}
+
+attributes = BEST_ATTRIBUTES[base_model.prediction]
+
+model = MultinomialNB(force_alpha=True, alpha=attributes['alpha'], fit_prior=attributes['fit_prior'])
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print(set(y_test) - set(y_pred))
+
+metrics = base_model.get_metrics(y_test, y_pred)
+
+with open(base_model.get_output_filepath('multinomial_naive_bayes'), 'w') as file:
+     file.write(json.dumps(metrics, indent=4))
+     file.write(str(model.get_params()))
+
+print('FINISHED after {} seconds'.format(time.time()-start))
