5e62c09b4acd33c61cbe63175d009185dd4a78ed
Stephanie Malvicini <s.a.m_puan@hotmail.com>
2024-02-19 10:44:56 +0100

    Upload experiments results, NN

--- Experiments/binary/ablation/neural_network.py
+++ Experiments/binary/ablation/neural_network.py
@@ -9,6 +9,8 @@ X_train, X_test, y_train, y_test = base_model.get_train_test_split()
 
 BEST_ATTRIBUTES = {
     '28': {'hidden_layer_sizes': (20,), 'batch_size': 200},
+    '30': {'hidden_layer_sizes': (20,), 'batch_size': 200},
+    '31': {'hidden_layer_sizes': (20,), 'batch_size': 200},
     '32': {'hidden_layer_sizes': (20,), 'batch_size': 200},
     '34': {'hidden_layer_sizes': (15,), 'batch_size': 200},
     '36': {'hidden_layer_sizes': (30,), 'batch_size': 100}
