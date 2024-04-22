3b9de080ca4a14bd683b189307a435169f375c32
Stephanie Malvicini <s.a.m_puan@hotmail.com>
2024-04-22 17:31:12 +0200

    Plots for experiment one, paper in progress

--- Experiments/plots_feature_selection.py
+++ Experiments/plots_feature_selection.py
@@ -6,19 +6,43 @@ import pandas as pd
 from matplotlib.patches import Patch
 
 RESULTS_FOLDER_PATH = 'binary/feature-selection/results-all(spread20,balanced)/'
-METRIC = 'f1'
+METRIC = 'precision'
 
 
 HATCHES = ['\\\\', '-', '//', '..', '', 'oo', '++', '||', 'XX', 'OO', '\\', '--', '/', '.', '+', 'o', '|', 'X', 'o']
 
 
-def prediction_display_name(prediction_original):
-    display_name = (prediction_original
-                    .replace('_', ' ')
-                    .replace('interval', '')
-                    .replace('predominant', '')
-                    .replace('mfd', 'MFD'))
-    return display_name
+def paper_experiment_one(dataframe):
+    features = {'emotion': 'Only emotion',
+                'emotion+linguistic-ratio': 'Emotion + Linguistic Ratio',
+                'personality': 'Only personality',
+                'linguistic-ratio+personality': 'Personality + Linguistic Ratio',
+                'sentiment': 'Only sentiment',
+                'linguistic-ratio+sentiment': 'Sentiment + Linguistic Ratio',
+                'emotion+personality': 'Emotion + Personality',
+                'emotion+linguistic-ratio+personality': 'Emotion + Personality + Linguistic Ratio',
+                'emotion+sentiment': 'Emotion + Sentiment',
+                'emotion+linguistic-ratio+sentiment': 'Emotion + Sentiment + Linguistic Ratio',
+                'personality+sentiment': 'Personality + Sentiment',
+                'linguistic-ratio+personality+sentiment': 'Personality + Sentiment + Linguistic Ratio',
+                'emotion+sentiment+personality': 'Emotion + Sentiment + Personality',
+                'emotion+linguistic-ratio+sentiment+personality': 'Emotion + Sentiment + Personality + Linguistic Ratio',
+                }
+    df = dataframe.loc[dataframe['feature'].isin(features.keys())]
+    df['feature'] = pd.Categorical(df['feature'], categories=features.keys(), ordered=True)
+    df['feature'] = df['feature'].replace(features)
+
+    predictions = {'predominant_emotion': 'P5',
+                   'predominant_sentiment': 'P4',
+                   'valence_ratio': 'P6',
+                   'abusive_ratio_interval': 'P1',
+                   'mfd_ratio': 'P3',
+                   'polarization_ratio_interval': 'P2'}
+    df['prediction'] = df['prediction'].replace(predictions)
+
+    df = df.sort_values(by=['feature', 'prediction'])
+    title = 'Precision: End to end, Spread = 20'
+    return df, title
 
 
 folders_list = os.listdir(RESULTS_FOLDER_PATH)
@@ -39,10 +63,11 @@ for folder in folders_list:
         feature = folder.replace('experiments-', '')
         for name, value in metrics.items():
             model_data = {'metric_name': name, 'metric_value': value, 'model': model,
-                          'prediction': prediction_display_name(prediction), 'spread': spread, 'feature': feature}
+                          'prediction': prediction, 'spread': spread, 'feature': feature}
             plot_data.append(model_data)
 
 dataframe = pd.DataFrame(plot_data)
+dataframe, title = paper_experiment_one(dataframe)
 dataframe = dataframe.loc[dataframe['metric_name'] == METRIC]
 
 sns.set_style("whitegrid")
@@ -54,7 +79,7 @@ p.set(xlabel=None, ylabel=None)
 p.set_titles("{col_name}")
 
 colors = []
-features = list(map(lambda name: name.replace('_', ' ').capitalize(), dataframe['feature'].unique()))
+features = dataframe['feature'].unique()
 
 # Loop through the bars and assign hatches
 for axes in p.axes.flat:
@@ -71,5 +96,7 @@ legend_patches = [Patch(facecolor=colors[i], edgecolor='black', hatch=HATCHES[i]
                   for i in range(0, len(colors))]
 plt.legend(handles=legend_patches, bbox_to_anchor=(1.04, 0.5), loc="center left", borderaxespad=0, fontsize=10)
 plt.subplots_adjust(right=0.6)
+
+plt.title(title)
 
 plt.show()
