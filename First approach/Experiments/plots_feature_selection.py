8fd964766b5b9dd7c03f1250ce06ba351da7a32e
StephanieMalvicini <s.a.m_puan@hotmail.com>
2024-06-07 18:56:12 +0200

    Plot feature selection

--- First approach/Experiments/plots_feature_selection.py
+++ First approach/Experiments/plots_feature_selection.py
@@ -9,8 +9,8 @@ from matplotlib.patches import Patch
 
 FOLDER_PATHS = ['binary/feature-selection/results-t_test-30_runs(spread20,balanced)/only-action/',
                 'binary/feature-selection/results-t_test-30_runs(spread20,balanced)/end-to-end/']
-METRIC = 'recall'
-TITLE = 'Recall - average of 30 runs'
+METRIC = 'f1'
+TITLE = 'F1 score - average of 30 runs'
 
 HATCHES = ['\\\\', '-', '//', '..', '', '++', 'oo', '||', 'XX', 'OO', '\\', '--', '/', '.', '+', 'o', '|', 'X', 'o']
 
@@ -36,17 +36,23 @@ def paper_experiment_one(dataframe):
     df['feature'] = pd.Categorical(df['feature'], categories=features.keys(), ordered=True)
     df['feature'] = df['feature'].replace(features)
 
-    predictions = {'predominant_emotion': 'P5',
-                   'predominant_sentiment': 'P4',
-                   'valence_ratio': 'P6',
-                   'abusive_ratio_interval': 'P1',
-                   'mfd_ratio': 'P3',
-                   'polarization_ratio_interval': 'P2'}
+    # predictions = {'predominant_emotion': 'P5',
+    #                'predominant_sentiment': 'P4',
+    #                'valence_ratio': 'P6',
+    #                'abusive_ratio_interval': 'P1',
+    #                'mfd_ratio': 'P3',
+    #                'polarization_ratio_interval': 'P2'}
+    predictions = {'predominant_emotion': 'Pred. Emotion',
+                   'predominant_sentiment': 'Pred. Sentiment',
+                   'valence_ratio': 'Valence R.',
+                   'abusive_ratio_interval': 'Abusive R.',
+                   'mfd_ratio': 'MFD R.',
+                   'polarization_ratio_interval': 'Polarization R.'}
     df['prediction'] = df['prediction'].replace(predictions)
 
     df = df.sort_values(by=['feature', 'prediction'])
 
-    df['experiment_type'] = df['experiment_type'].str.replace('-', ' ').str.capitalize()
+    df['approach'] = df['approach'].str.replace('-', ' ').str.capitalize()
 
     return df
 
@@ -71,17 +77,17 @@ for file in file_list:
     f1_values = []
     recall_values = []
     precision_values = []
-    experiment_type = file.split('/')[3]
+    approach = file.split('/')[3]
     for instance in k_metrics:
         f1_values.append(instance['f1'])
         recall_values.append(instance['recall'])
         precision_values.append(instance['precision'])
     plot_data.append({'metric_name': 'f1', 'metric_value': mean(f1_values), 'prediction': prediction,
-                      'feature': feature, 'experiment_type': experiment_type})
+                      'feature': feature, 'approach': approach})
     plot_data.append({'metric_name': 'recall', 'metric_value': mean(recall_values), 'prediction': prediction,
-                      'feature': feature, 'experiment_type': experiment_type})
+                      'feature': feature, 'approach': approach})
     plot_data.append({'metric_name': 'precision', 'metric_value': mean(precision_values), 'prediction': prediction,
-                      'feature': feature, 'experiment_type': experiment_type})
+                      'feature': feature, 'approach': approach})
 
 dataframe = pd.DataFrame(plot_data)
 dataframe = paper_experiment_one(dataframe)
@@ -89,9 +95,10 @@ dataframe = dataframe.loc[dataframe['metric_name'] == METRIC]
 
 sns.set_style("whitegrid")
 
-p = sns.catplot(data=dataframe, x="prediction", y="metric_value", hue="feature", kind="bar", col='experiment_type',
+p = sns.catplot(data=dataframe, x="prediction", y="metric_value", hue="feature", kind="bar", col='approach',
                 edgecolor='black', legend=False)
 p.set(xlabel=None, ylabel=None)
+p.set_axis_labels('', TITLE)
 
 colors = []
 features = dataframe['feature'].unique()
@@ -111,6 +118,7 @@ legend_patches = [Patch(facecolor=colors[i], edgecolor='black', hatch=HATCHES[i]
                   for i in range(0, len(colors))]
 plt.legend(handles=legend_patches,  borderaxespad=0, fontsize=10,
            ncols=4, loc='lower center', bbox_to_anchor=(-1, -0.25, 2, 1))
-plt.suptitle(TITLE, size=16)
+# plt.suptitle(TITLE, size=16)
+plt.suptitle('Spread: 20 - Words present: False - Balanced: True', size=16)
 plt.subplots_adjust(top=0.9, bottom=0.2)
 plt.show()
