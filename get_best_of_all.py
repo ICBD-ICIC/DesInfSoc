420f5c5882eb9f2f0666726bbc2c0bb67ce96b89
StephanieMalvicini <s.a.m_puan@hotmail.com>
2024-06-07 16:53:20 +0200

    Plot best experiment per prediction

--- get_best_of_all.py
+++ get_best_of_all.py
@@ -2,6 +2,10 @@ import json
 import os
 
 import pandas as pd
+from matplotlib import pyplot as plt
+
+pd.set_option("max_colwidth", 200)
+pd.set_option("display.max_columns", None)
 
 BINARY_FOLDERS = [
     'Experiments/binary/experiments(spread20,balanced,only-action)',
@@ -29,36 +33,101 @@ def get_all_results_files(folder_paths):
 
 def get_metrics_values(file_list, metrics_list):
     metrics_values = []
+    print('Total', len(file_list))
 
-    for file in file_list:
-        metrics = open(file, "r").read()
+    for file_path in file_list:
+        metrics = open(file_path, "r").read()
         metrics = " ".join(metrics.split())
         metrics = metrics.split('}{', 1)[0]
         metrics = metrics + '}' if metrics[-1] != '}' else metrics
         metrics = json.loads(metrics)
-        prediction = file.replace('\\', '/').split('/')[-1].split(',')[2]
+        filename = file_path.replace('\\', '/').split('/')[-1]
+        model = filename.split(',')[1]
+        prediction = filename.split(',')[2]
+        spread = filename.split(',')[0].split('_')[1].split('-')[-1].replace('SPREAD', '')
+        if len(filename.split(',')[0].split('_')[1].split('-')) > 1:
+            approach = 'only-action'
+        else:
+            approach = 'end-to-end'
+        if 'First approach' in file_path:
+            words_present = False
+        else:
+            words_present = True
+        if 'imbalanced' in file_path:
+            class_balance = False
+        else:
+            class_balance = True
         metrics = {key: metrics[key] for key in metrics_list}
         for name, value in metrics.items():
-            model_data = {f"{name}": value, 'file': file, 'prediction': prediction}
+            model_data = {f"{name}": value, 'file_path': file_path, 'prediction': prediction,
+                          'model': model.replace('_', ' ').capitalize(),
+                          'spread': spread, 'approach': approach, 'words_present': words_present,
+                          'class_balance': class_balance}
             metrics_values.append(model_data)
 
     return pd.DataFrame(metrics_values)
 
 
+def get_plot_title(best, metric_name):
+    prediction = best['prediction'].replace('_', ' ').capitalize().replace('Mfd', 'MFD')
+    return (f"Best {metric_name.replace('_', ' ').capitalize()} score for {prediction}",
+            f"Spread: {best['spread']} - Approach: {best['approach'].replace('-', ' ').capitalize()} - "
+            f"Words present: {best['words_present']} - Balanced: {best['class_balance']}")
+
+
+def get_plot(df, best_idx, metric_name, metrics_names):
+    columns_to_drop = metrics_names + ['file_path', 'model']
+    filtering_criteria = df.iloc[best_idx].drop(columns_to_drop)
+
+    filtering_condition = pd.Series([True] * len(df))
+    for col, val in filtering_criteria.items():
+        filtering_condition &= (df[col] == val)
+
+    filtered_df = df[filtering_condition]
+
+    fig, ax = plt.subplots(figsize=(10, 5))
+    bars = ax.barh(filtered_df['model'], filtered_df[metric_name])
+    # Remove axes splines
+    for s in ['top', 'bottom', 'left', 'right']:
+        ax.spines[s].set_visible(False)
+    ax.xaxis.set_ticks_position('none')
+    ax.yaxis.set_ticks_position('none')
+    ax.xaxis.set_tick_params(pad=5)
+    ax.yaxis.set_tick_params(pad=10)
+    ax.grid(color='black',
+            linestyle='-', linewidth=1,
+            alpha=0.2)
+    ax.set_xlabel(f"{metric_name.replace('_', ' ').capitalize()}")
+    title, subtitle = get_plot_title(df.iloc[best_idx], metric_name)
+    fig.suptitle(title)
+    ax.set_title(subtitle, loc='center')
+    plt.subplots_adjust(left=0.25, top=0.85)
+    max_value = filtered_df[metric_name].max()
+    for bar in bars:
+        width = bar.get_width()
+        if width == max_value:
+            bar.set_color('red')
+        ax.text(width + 0.01 * width, bar.get_y() + bar.get_height() / 2, f'{width:.2f}',
+                va='center', ha='left')
+    plt.show()
+
+
 # BINARY #
 print('BINARY')
-df = get_metrics_values(get_all_results_files(BINARY_FOLDERS), ['f1'])
+metrics_names = ['f1']
+df = get_metrics_values(get_all_results_files(BINARY_FOLDERS), metrics_names)
 for prediction in df['prediction'].unique():
     best_result_idx = df.loc[df['prediction'] == prediction]['f1'].idxmax()
-    print('Best f1 score for', prediction, 'is on file', df.iloc[best_result_idx]['file'])
+    get_plot(df, best_result_idx, 'f1', metrics_names)
 
 # MULTICLASS #
 print('MULTICLASS')
-df = get_metrics_values(get_all_results_files(MULTICLASS_FOLDERS), ['f1_micro', 'f1_macro', 'f1_weighted'])
+metrics_names = ['f1_micro', 'f1_macro', 'f1_weighted']
+df = get_metrics_values(get_all_results_files(MULTICLASS_FOLDERS), metrics_names)
 for prediction in df['prediction'].unique():
     best_result_idx = df.loc[df['prediction'] == prediction]['f1_micro'].idxmax()
-    print('Best f1_micro score for', prediction, 'is on file', df.iloc[best_result_idx]['file'])
+    get_plot(df, best_result_idx, 'f1_micro', metrics_names)
     best_result_idx = df.loc[df['prediction'] == prediction]['f1_macro'].idxmax()
-    print('Best f1_macro score for', prediction, 'is on file', df.iloc[best_result_idx]['file'])
+    get_plot(df, best_result_idx, 'f1_macro', metrics_names)
     best_result_idx = df.loc[df['prediction'] == prediction]['f1_weighted'].idxmax()
-    print('Best f1_weighted score for', prediction, 'is on file', df.iloc[best_result_idx]['file'])
+    get_plot(df, best_result_idx, 'f1_weighted', metrics_names)
