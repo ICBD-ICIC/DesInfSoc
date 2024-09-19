1ebff7793cb4ec8ebd7b6cae258c3bd2904aa547
Stephanie Malvicini <s.a.m_puan@hotmail.com>
2024-09-19 12:18:44 +0200

    Climate change metrics and ideas

---
+++ climate_change/totals.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data1 = pd.read_csv('data1.csv', delimiter=';')
+data2 = pd.read_csv('data2.csv', delimiter=';')
+data3 = pd.read_csv('data3.csv', delimiter=';')
+
+df = pd.concat([data1, data2, data3], ignore_index=True, sort=False).dropna()
+result = df.groupby('user')['amount'].sum().reset_index().sort_values('amount')
+result['amount'] = result['amount'].astype(int)
+
+result.to_csv('all.csv', index=False)
+
+print(result)
