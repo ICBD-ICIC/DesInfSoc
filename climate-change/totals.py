c9367ad6406e64eb71ba70a347bab4f6eac99069
Stephanie Malvicini <s.a.m_puan@hotmail.com>
2024-09-26 13:02:28 +0200

    Refactor project

--- climate-change/totals.py
+++ climate-change/totals.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-data1 = pd.read_csv('data1.csv', delimiter=';')
-data2 = pd.read_csv('data2.csv', delimiter=';')
-data3 = pd.read_csv('data3.csv', delimiter=';')
+data1 = pd.read_csv('dataset/data1.csv', delimiter=';')
+data2 = pd.read_csv('dataset/data2.csv', delimiter=';')
+data3 = pd.read_csv('dataset/data3.csv', delimiter=';')
 
 df = pd.concat([data1, data2, data3], ignore_index=True, sort=False).dropna()
 result = df.groupby('user')['amount'].sum().reset_index().sort_values('amount')
