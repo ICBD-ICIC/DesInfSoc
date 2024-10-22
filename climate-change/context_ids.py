7ae7ffc9cba74ee741412b99d572dd2e271e70a0
StephanieMalvicini <s.a.m_puan@hotmail.com>
2024-10-22 15:13:29 +0200

    Get context and prediction tweets ids

--- climate-change/context_ids.py
+++ climate-change/context_ids.py
@@ -5,7 +5,7 @@ CONTEXT_TWEETS = 'dataset/context_tweets_pattern_matching.csv'
 USERS = 'dataset/users_min_10.csv'
 REPLIES = 'dataset/replies.csv'
 INFLUENCERS = 'dataset/influencers.csv'
-OUTPUT_FILE = 'dataset/input_and_ground_truth.csv'
+OUTPUT_FILE = 'outputs/input_and_ground_truth.csv'
 
 usernames = pd.read_csv(USERS)['user']
 replies = pd.read_csv(REPLIES)
@@ -19,11 +19,12 @@ for username in usernames:
         conversation_id = reply['conversation_id']
         original_tweet = influencers[influencers['id'] == conversation_id]
         all_tweets_conversation = replies[replies['conversation_id'] == conversation_id]
-        previous_posts_ids = all_tweets_conversation[all_tweets_conversation['created_at'] < reply['created_at']]['id'].to_list()
-        previous_posts_ids.append(original_tweet['id'])
-        input_and_ground_truth.append({'username': username,
-                                       'user_reply_id': reply['id'],
-                                       'previous_posts_ids': previous_posts_ids})
-
+        previous_posts_ids = original_tweet['id'].to_list()
+        previous_posts_ids += all_tweets_conversation[all_tweets_conversation['created_at'] < reply['created_at']]['id'].to_list()
+        if len(previous_posts_ids) > 0:
+            input_and_ground_truth.append({'username': username,
+                                           'user_reply_id': reply['id'],
+                                           'previous_posts_ids': previous_posts_ids})
 input_and_ground_truth_df = pd.DataFrame(input_and_ground_truth)
 input_and_ground_truth_df.to_csv(OUTPUT_FILE, index=False)
+print(len(input_and_ground_truth_df.index))
