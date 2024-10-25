c410b041b532abfcc62de834258a9e47fcde4480
StephanieMalvicini <s.a.m_puan@hotmail.com>
2024-10-25 10:40:46 +0200

    Improve distance for polar words performance

--- climate-change/metrics/polarization_distance.py
+++ climate-change/metrics/polarization_distance.py
@@ -10,20 +10,26 @@ from sklearn.metrics.pairwise import cosine_similarity
 from ast import literal_eval
 
 already_found_words = []
+already_checked = []
 
 # Dictionary is a list of embeddings with the dictionary words for the linguistic feature being calculated
 def polar_words(text, dictionary):
     polar_or_similar = []
     for word in text:
+        found = False
         if word in already_found_words:
             polar_or_similar.append(word)
-        else:
+            found = True
+        elif not word in already_checked:
             word_embedding = ft.get_word_vector(word).reshape(1, -1)
             for dictionary_word in dictionary:
                 if cosine_similarity(word_embedding, dictionary_word) >= 0.6:
                     polar_or_similar.append(word)
                     already_found_words.append(word)
+                    found = True
                     break
+        if not found:
+            already_checked.append(word)
     return " ".join(polar_or_similar)
 
 MODEL_PATH = '../../../crawl-300d-2M-subword.bin'
@@ -31,9 +37,15 @@ CONTEXT_CLEANED_TWEETS_FILE = '../dataset/context_tweets_distance.csv'
 POLARIZATION_WORDS_DICTIONARY = 'dictionaries/lang_online_polarization_dict.csv'
 OUTPUT_FILE = '../outputs/polar_{0}.csv'.format(time.time())
 
+# Paths in the kluster
+# MODEL_PATH = 'crawl-300d-2M-subword.bin'
+# CONTEXT_CLEANED_TWEETS_FILE = 'dataset/context_tweets_distance.csv'
+# POLARIZATION_WORDS_DICTIONARY = 'dictionaries/lang_online_polarization_dict.csv'
+# OUTPUT_FILE = 'outputs/polar_{0}.csv'.format(time.time())
+
 ft = fasttext.load_model(MODEL_PATH)
 
-all_tweets = pd.read_csv(CONTEXT_CLEANED_TWEETS_FILE, converters={"clean_text": literal_eval})
+all_tweets = pd.read_csv(CONTEXT_CLEANED_TWEETS_FILE, converters={"clean_text": literal_eval})[0:10]
 
 start = time.time()
 polarization_dictionary = pd.read_csv(POLARIZATION_WORDS_DICTIONARY)
@@ -48,3 +60,4 @@ all_tweets = all_tweets.drop(columns=['clean_text'])
 all_tweets.to_csv(OUTPUT_FILE, index=False)
 
 print('FINISHED after {} seconds'.format(time.time()-start))
+print(already_checked)
--- climate-change/metrics/polarization_distance.sh
+++ climate-change/metrics/polarization_distance.sh
@@ -3,9 +3,9 @@
 #SBATCH --mem-per-cpu=16G
 #SBATCH --cpus-per-task=1
 #SBATCH --time=01-00:00:00
-#SBATCH --output=outputs/polarization-%A-%a.out
-#SBATCH --error=errors/polarization-%A-%a.err
+#SBATCH --output=outputs/polarization-%j.out
+#SBATCH --error=errors/polarization-%j.err
 
-source ../.experiments_env/bin/activate
+source .experiments_env/bin/activate
 
 srun python polarization_distance.py
