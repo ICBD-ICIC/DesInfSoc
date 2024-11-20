dd06c6eafecf38c9e71a4ba5a5122d534c354b94
StephanieMalvicini <s.a.m_puan@hotmail.com>
2024-11-20 13:16:12 +0100

    Remove unused files

--- climate-change/lit/main.py
+++
@@ -1,9 +0,0 @@
-import lit_nlp
-
-datasets = {
-    'foo_data': FooDataset('/path/to/foo.tsv'),
-    'bar_data': BarDataset('/path/to/bar.tfrecord'),
-}
-models = {'my_model': MyModel('/path/to/model/files')}
-lit_demo = lit_nlp.dev_server.Server(models, datasets, port=4321)
-lit_demo.serve()
